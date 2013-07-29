04a1645848bbb033f54b24d08dc6f5814bfb07ea
aligarian <ammar.hayder@gmail.com>
2013-07-29 12:32:39 +0530

    Update cart_manage_view.php

--- application/views/admin/cart_manage_view.php
+++ application/views/admin/cart_manage_view.php
@@ -2,7 +2,7 @@
 
 	<a href="<?=site_url('cart/manage/1');?>" class="cart_1" id="cart_1" style="margin:10px;font-weight:bold;">Manage Cart One</a></br>
 
-	<a href="<?=site_url('cart/manage/2');?>" id="cart_2" style="margin:10px;font-weight:bold;">Manage Cart Two</a></br>
+	<a href="<?=site_url('cart/manage/2');?>" class="cart_2" id="cart_2" style="margin:10px;font-weight:bold;">Manage Cart Two</a></br>
 
-	<a href="<?=site_url('cart/manage/3');?>" id="cart_3" style="margin:10px;font-weight:bold;">Manage Cart Three</a></br>
+	<a href="<?=site_url('cart/manage/3');?>" class="cart_3" id="cart_3" style="margin:10px;font-weight:bold;">Manage Cart Three</a></br>
 </div>
